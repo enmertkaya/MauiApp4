92014fba3968600fbef3fde3dda76a61b6a9f5ed
Enes Mert Kaya <151652097+enmertkaya@users.noreply.github.com>
2025-01-28 15:10:44 +0300

    Add project files.

---
+++ MauiApp4/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Org.Apache.Http.Util;
+
+namespace MauiApp4
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnGoToPage2_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Page2(EntUserName.Text));
+        }
+    }
+
+}
---
+++ MauiApp4/Page2.xaml.cs
@@ -0,0 +1,11 @@
+namespace MauiApp4;
+
+public partial class Page2 : ContentPage
+{
+	public Page2(string userName)
+	{
+		InitializeComponent();
+		LblUserName.Text = userName;
+
+    }
+}
