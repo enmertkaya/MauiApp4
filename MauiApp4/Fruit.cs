6a59220bc27ef62066e03f5d9cea8eef5d1fe566
Enes Mert Kaya <151652097+enmertkaya@users.noreply.github.com>
2025-01-29 15:51:26 +0300

    data source and data binding

---
+++ MauiApp4/Fruit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp4
+{
+    public class Fruit
+    {
+        public string Name { get; set; }
+        public string ImageName { get; set; }
+        public string Sentence { get; set; }
+    }
+}
--- MauiApp4/MainPage.xaml.cs
+++ MauiApp4/MainPage.xaml.cs
@@ -4,10 +4,24 @@ namespace MauiApp4
 {
     public partial class MainPage : ContentPage
     {
-        List<string> FruitsList = new List<string>()
-        {
-            "Apple","Banana","Cocunut","Grape","Guava","Kiwi","Mango","Melon"
-        };
+        List<Fruit> FruitsList = new List<Fruit>()
+{
+    new Fruit() {Name = "Apple", ImageName = "apple.png", Sentence = "Elmalı turtalarımızı denediniz mi?"},
+    new Fruit() {Name = "Apricot", ImageName = "apricot.png", Sentence = "Kayısı reçelimizi denediniz mi?"},
+    new Fruit() {Name = "Banana", ImageName = "banana.png", Sentence = "Muzlu milkshake'imiz harika olur!"},
+    new Fruit() {Name = "Coconut", ImageName = "coconut.png", Sentence = "Hindistan cevizli tatlarımızı denemelisiniz."},
+    new Fruit() {Name = "Grape", ImageName = "grape.png", Sentence = "Üzüm suyu doğal ve taze!"},
+    new Fruit() {Name = "Guava", ImageName = "guava.png", Sentence = "Guava meyveli smoothiemiz favori!"},
+    new Fruit() {Name = "Kiwi", ImageName = "kiwi.png", Sentence = "Kivi pastamız harika bir tat!"},
+    new Fruit() {Name = "Mango", ImageName = "mango.png", Sentence = "Mangolu tatlarımız yaz havası getirir!"},
+    new Fruit() {Name = "Melon", ImageName = "melon.png", Sentence = "Kavun salatalarımıza göz atın."},
+    new Fruit() {Name = "Orange", ImageName = "orange.png", Sentence = "Portakal suyuyla tazelenin!"},
+    new Fruit() {Name = "Peach", ImageName = "peach.png", Sentence = "Şeftalili tartlarımızı çok seveceksiniz!"},
+    new Fruit() {Name = "Pear", ImageName = "pear.png", Sentence = "Armut tatlılarımızı denemeye ne dersiniz?"},
+    new Fruit() {Name = "Pineapple", ImageName = "pineapple.png", Sentence = "Ananaslı pizzamız favoriniz olacak."},
+    new Fruit() {Name = "Plum", ImageName = "plum.png", Sentence = "Erikli keklerimiz tam size göre."}
+};
+
 
         public MainPage()
         {
